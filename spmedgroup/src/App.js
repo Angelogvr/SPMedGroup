0ee1d7be466258f5803bb7cfdebd3a890524967d
Angelo Gabriel <angelogabrielvr@gmail.com>
2021-11-11 11:08:06 -0300

    Front iniciado

---
+++ spmedgroup/src/App.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+render()
+
+export default App;
